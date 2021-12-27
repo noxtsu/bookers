8abb50c3ff0efcc63df87e1ba6ecfb6b27d9eb35
EC2 Default User <ec2-user@ip-172-31-84-239.ec2.internal>
2021-12-27 11:35:16 +0000

    test1 6 failures

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,8 +16,12 @@ class BooksController < ApplicationController
   
   def create 
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+     redirect_to book_path(@book.id), notice: "Book was successfully created."
+    else @books = Book.all 
+      render :index
+    
+    end
   end
 
   def edit
@@ -26,8 +30,10 @@ class BooksController < ApplicationController
   
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    else render :edit
+    end
   end
 
   
